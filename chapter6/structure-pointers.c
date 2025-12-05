dc466fc2a37adec3a0549c6e6cd7b0993e2b1c5b
Namraj Pudasaini <namraj@Namrajs-MacBook-Pro.local>
2025-12-05 12:22:31 +1100

    structure pointers

---
+++ chapter6/structure-pointers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct {
+    int len;
+    char *str;
+    } *p ;
+
+int main(void) {
+    p = malloc(sizeof(*p)); // allocate memory for the struct
+    //  p = {6,"hello"};// invalid: p is a pointer to an unnamed struct type. To use {6, "hello"}, you would need an actual struct object to assign to
+    // struct point = {6,"hello"}; // invalid
+    p->len = 6;     // means: (*p).len
+    p->str = "hello";   // means: (*p).str
+    printf("string: %c \n", *p->str);
+    printf("length:%d \n", p->len);
+
+    free(p);
+    return 0;
+}
