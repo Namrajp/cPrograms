e26efd9c5cbffff075ce31a09e0ce3d433b9595c
Namraj Pudasaini <namraj@Namrajs-MacBook-Pro.local>
2026-02-01 22:55:10 +1100

    sqeeze operator puls plus

---
+++ chapter2/squeeze.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+void squeeze(char s[], int c);
+
+int main(void)
+{
+    char r[] = {'c','a','t','i','s'};
+   squeeze(r, 'c');
+
+    for (int m = 0; m < strlen(r); m++)
+    {
+        printf("%c", r[m]);
+    }
+    printf("\n");
+}
+
+void squeeze(char x[], int y)
+{
+    int i, j;
+    for( j=0, i = 0; x[i] != '\0'; i++)
+    {
+        if (x[i] != y)
+        {
+            x[j++] = x[i];
+        }
+    }
+    x[j] = '\0';
+
+}
